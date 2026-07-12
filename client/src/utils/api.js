308f24bb01a9e2358df60a6bf2f1d0d136eaa189
sivadev ss <ssh.shivadev@gmail.com>
2026-07-12 13:35:53 +0530

    Connect frontend to Render backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5604/api',
+    baseURL: 'https://digitaltwin-lond.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
